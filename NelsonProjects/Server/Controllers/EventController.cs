7e04b901f8fb6a96a5f3f6cd64e7ee70e1fbf060
Spencer Nelson <snelson@heliotraining.com>
2020-09-30 10:01:46 -0600

    start of project

---
+++ NelsonProjects/Server/Controllers/EventController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NelsonProjects.Shared;
+
+namespace NelsonProjects.Server.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class EventController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Event> Get()
+        {
+            var listOfEvents = new List<Event>();
+
+            listOfEvents.Add(new Event() { Date = new DateTime(2020, 12, 25), Description = "Test is True" });
+            listOfEvents.Add(new Event() { Date = DateTime.Now, Description = "Test is false" });
+            listOfEvents.Add(new Event() { Date = new DateTime(2020, 3, 15), Description = "Test is false" });
+
+            return listOfEvents;
+        }
+    }
+}
---
+++ NelsonProjects/Shared/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NelsonProjects.Shared
+{
+    public class Event
+    {
+        public DateTime Date { get; set; }
+
+        public string Description { get; set; }
+        
+        public string Location { get; set; }
+
+        public IEnumerable<string> AttendingList { get; set; }
+    }
+}
