e76148fc4bfb896e04fa695602ae15cb5bb30a89
spencernelson144@gmail.com <snelson@heliotraining.com>
2020-10-01 08:11:57 -0600

    fixed connection string for heroku

--- NelsonProjects/Shared/DAL/SqlDataAccess.cs
+++ NelsonProjects/Shared/DAL/SqlDataAccess.cs
@@ -32,7 +32,19 @@ namespace NelsonProjects.Shared.DAL
             }
             else
             {
-                _connectionString = Environment.GetEnvironmentVariable("DATABASE_URL");
+                var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                // Parse connection URL to connection string for Npgsql
+                connUrl = connUrl.Replace("postgres://", string.Empty);
+                var pgUserPass = connUrl.Split("@")[0];
+                var pgHostPortDb = connUrl.Split("@")[1];
+                var pgHostPort = pgHostPortDb.Split("/")[0];
+                var pgDb = pgHostPortDb.Split("/")[1];
+                var pgUser = pgUserPass.Split(":")[0];
+                var pgPass = pgUserPass.Split(":")[1];
+                var pgHost = pgHostPort.Split(":")[0];
+                var pgPort = pgHostPort.Split(":")[1];
+                _connectionString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
             }
         }
 
